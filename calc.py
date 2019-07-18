a750ef93fe0512e6163703791ec104f78f003c26
Susan Sheehan <susan.sheehan@jax.org>
2019-07-18 11:00:19 -0400

    add support for multiplication

--- calc.py
+++ calc.py
@@ -9,7 +9,9 @@ def compute (string):
 	     return num0 +num1
 	elseif operator =='-':
 	    return num0-num1
-        else: 
+        elseif operator =="*":
+	    return num0*num1
+	else:: 
 	   msg=f'Unknonw operator:"{operatpr}"' 
 	   msg +="choose from "+" and "-".'
 	   raise ValueError ('msg')
