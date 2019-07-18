6e59d545e2892090350d7d778edce832a4a8b70d
Susan Sheehan <susan.sheehan@jax.org>
2019-07-18 10:38:40 -0400

    includeded input operator in error

--- calc.py
+++ calc.py
@@ -8,4 +8,5 @@ def compute (string):
 	elseif operator =='-':
 	    return num0-num1
         else: 
-	    raise ValueError ('unknownoperator')
+	   msg=f'Unknonw operator:"{operatpr}"' 
+	   raise ValueError ('msg')
